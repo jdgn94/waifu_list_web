25948ded7b67fdb168fdb1d342c4eaa8b437a214
Juan Daniel Gonzalez Noriega <jdgn94@gmail.com>
2025-01-06 12:02:47 -0400

    feat: :hammer: route
    create page to search waifus and show info
    
    todo: create and edit waifu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,45 +1,83 @@
 <template>
   <v-app-bar color="primary" :elevation="2">
     <template #prepend>
-      <v-app-bar-nav-icon />
+      <v-app-bar-nav-icon class="mobile-menu" @click="menu = true" />
     </template>
 
     <v-app-bar-title>Waifu List</v-app-bar-title>
 
     <v-spacer />
 
-    <v-btn to="/waifus">
+    <v-btn class="desktop-menu" to="/waifus">
       <span>Waifus</span>
     </v-btn>
-    <v-btn>
+    <v-btn class="desktop-menu">
       <span>Franchises</span>
     </v-btn>
-    <v-btn>
+    <v-btn class="desktop-menu">
       <span>Types</span>
     </v-btn>
-    <v-btn>
+    <v-btn class="desktop-menu">
       <span>Rarity</span>
     </v-btn>
     <v-divider vertical />
     <!-- log in visible if token == null -->
-    <v-btn>
+    <v-btn class="desktop-menu">
       <span>Log In</span>
     </v-btn>
     <!-- buttons visible if token != null -->
-    <v-btn>
+    <v-btn class="desktop-menu">
       <span>My List</span>
     </v-btn>
-    <v-btn>
+    <v-btn class="desktop-menu">
       <span>Favorite List</span>
     </v-btn>
-    <v-btn>
+    <v-btn class="desktop-menu">
       <span>Log Out</span>
     </v-btn>
   </v-app-bar>
+
+  <v-bottom-sheet
+    v-model="menu"
+    close-on-content-click
+    inset
+    location-strategy="connected"
+  >
+    <v-card>
+      <v-list>
+        <v-list-item to="/waifus">
+          <span>Waifus</span>
+        </v-list-item>
+        <v-list-item>
+          <span>Franchises</span>
+        </v-list-item>
+        <v-list-item>
+          <span>Types</span>
+        </v-list-item>
+        <v-list-item>
+          <span>Rarity</span>
+        </v-list-item>
+        <v-divider />
+        <v-list-item>
+          <span>Log In</span>
+        </v-list-item>
+        <v-list-item>
+          <span>My List</span>
+        </v-list-item>
+        <v-list-item>
+          <span>Favorite List</span>
+        </v-list-item>
+        <v-list-item>
+          <span>Log Out</span>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-bottom-sheet>
 </template>
 
 <script lang="ts" setup>
   const token = ref(null) as Ref<string | null>
+  const menu = ref(false)
 
   onMounted(() => {
     token.value = localStorage.getItem('token')
--- src/pages/waifus/index.vue
+++ src/pages/waifus/index.vue
@@ -1,26 +1,60 @@
 <template>
-  <div class="card-container">
-    <WaifuCard
-      v-for="waifu in waifus"
-      :key="waifu.id"
-      :data="waifu"
+  <div class="position-relative">
+    <div class="search-container">
+      <v-text-field
+        v-model="name"
+        clearable
+        hide-details
+        label="Name or Nickname"
+        prepend-inner-icon="mdi-magnify"
+        variant="solo"
+      />
+      <v-divider vertical />
+      <v-autocomplete
+        v-model="franchiseId"
+        clearable
+        hide-details
+        item-title="name"
+        item-value="id"
+        :items="franchises"
+        label="Franchise"
+        variant="solo"
+      />
+    </div>
+    <v-fab
+      absolute
+      class="floating-button"
+      color="primary"
+      extended
+      location="end"
+      position="fixed"
+      prepend-icon="mdi-plus"
+      size="64"
+      z-index="10"
     />
-  </div>
-  <div class="text-center">
-    <v-container>
-      <v-row justify="center">
-        <v-col cols="8">
-          <v-container class="max-width">
-            <v-pagination
-              v-model="page"
-              class="my-4"
-              :length="totalPages"
-              smart="7"
-            />
-          </v-container>
-        </v-col>
-      </v-row>
-    </v-container>
+    <div class="card-container">
+      <WaifuCard
+        v-for="waifu in waifus"
+        :key="waifu.id"
+        :data="waifu"
+      />
+    </div>
+    <div class="text-center">
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="8">
+            <v-container class="max-width">
+              <v-pagination
+                v-model="page"
+                class="my-4"
+                :length="totalPages"
+                smart="7"
+              />
+            </v-container>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
   </div>
 </template>
 
@@ -28,34 +62,82 @@
   import router from '@/router'
   import { WaifuList } from '@/interfaces/waifu'
   import WaifuCard from '@/components/WaifuCard.vue'
+  import { Franchise } from '@/interfaces/franchise'
   const waifus = ref([] as WaifuList[])
+  const franchises = ref([] as Franchise[])
   const page = ref(parseInt(router.currentRoute.value.query.page?.toString() ?? '1'))
+  const name = ref(router.currentRoute.value.query.name?.toString() ?? '')
+  const franchiseDefaultValue = parseInt(router.currentRoute.value.query.franchise?.toString() ?? '0')
+  const franchiseId = ref(franchiseDefaultValue > 0 ? franchiseDefaultValue : null)
   const totalPages = ref(0)
   const loading = ref(false)
+  const timeout = ref(null as any)
   const baseUrl = import.meta.env.VITE_BASE_URL_API
 
   interface GetWaifus {
-    page?: number;
+    page: number;
     name?: string;
+    franchise?: number | null;
   }
 
   onMounted(() => {
     loading.value = true
-    console.log(import.meta.env.DEV)
-    console.log(import.meta.env)
-    getWaifus({ page: page.value })
+    getFranchises()
+    getWaifus({ page: page.value, name: name.value, franchise: franchiseId.value })
   })
 
   // watches
   watch(page, (newValue, oldValue) => {
     if (newValue !== oldValue) {
-      getWaifus({ page: page.value })
-      router.push({ path: '/waifus', query: { page: newValue } })
+      if (timeout.value) clearTimeout(timeout.value)
+
+      getWaifus({ page: newValue, name: name.value, franchise: franchiseId.value })
+      router.push({ path: '/waifus', query: { page: newValue, name: name.value, franchise: franchiseId.value } })
     }
   })
 
+  watch(name, (newValue, oldValue) => {
+    if (newValue !== oldValue) {
+      if (timeout.value) clearTimeout(timeout.value)
+
+      timeout.value = setTimeout(() => {
+        page.value = 1
+        getWaifus({ page: 1, name: newValue, franchise: franchiseId.value })
+        router.push({ path: '/waifus', query: { page: 1, name: newValue, franchise: franchiseId.value } })
+      }, 500)
+    }
+  })
+
+  watch(franchiseId, (newValue, oldValue) => {
+    if (newValue !== oldValue) {
+      if (timeout.value) clearTimeout(timeout.value)
+
+      page.value = 1
+      getWaifus({ page: page.value, name: name.value, franchise: newValue })
+      router.push({ path: '/waifus', query: { page: 1, name: name.value, franchise: newValue } })
+    }
+  })
+
+  // functions
+  const getFranchises = async () => {
+    const url = `${baseUrl}/franchises`
+    const response = await fetch(
+      url,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    if (response.ok) {
+      const values = await response.json()
+      franchises.value = values.data as Franchise[]
+    }
+  }
+
   const getWaifus = async (params: GetWaifus) => {
-    const newLocal = `${baseUrl}/waifus?page=${params.page ?? 1}&name=${params.name ?? ''}`
+    const newLocal = `${baseUrl}/waifus?page=${params.page ?? 1}&name=${params.name ?? ''}&franchise=${params.franchise ?? ''}`
     const response = await fetch(
       newLocal,
       {
